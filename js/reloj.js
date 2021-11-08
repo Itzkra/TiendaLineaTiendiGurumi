117c18151b014dc902f59ce3953b0d123ad12ae5
Itzkra <83843365+Itzkra@users.noreply.github.com>
2021-11-08 15:30:00 -0600

    segundo commit

--- js/reloj.js
+++ js/reloj.js
@@ -1,5 +1,5 @@
 let mostrarFecha = document.getElementById('fecha');
-let mostrarReloj = document.getElementById('reloj');
+
 
 let fecha = new Date();
 
@@ -9,9 +9,49 @@ let mesAnyo = ['01','02', '03','04','05','06','07','08','09','10','11','12'];
 
 mostrarFecha.innerHTML = ` ${fecha.getDate()} / ${mesAnyo[fecha.getMonth()]} / ${fecha.getFullYear()}`;
 
+function cargarReloj(){
+
+    // Haciendo uso del objeto Date() obtenemos la hora, minuto y segundo 
+    var fechahora = new Date();
+    var hora = fechahora.getHours(); 
+    var minuto = fechahora.getMinutes(); 
+    var segundo = fechahora.getSeconds(); 
+
+    // Variable meridiano con el valor 'AM' 
+    var meridiano = "AM";
 
 
-setInterval(()=>{
-  let hora = new Date();
-  mostrarReloj.innerHTML = hora.toLocaleTimeString()
-},1000);
+    // Si la hora es igual a 0, declaramos la hora con el valor 12 
+    if(hora == 0){
+
+        hora = 12;
+
+    }
+
+    // Si la hora es mayor a 12, restamos la hora - 12 y mostramos la variable meridiano con el valor 'PM' 
+    if(hora > 12) {
+
+        hora = hora - 12;
+
+        // Variable meridiano con el valor 'PM' 
+        meridiano = "PM";
+
+    }
+
+    // Formateamos los ceros '0' del reloj 
+    hora = (hora < 10) ? "0" + hora : hora;
+    minuto = (minuto < 10) ? "0" + minuto : minuto;
+    segundo = (segundo < 10) ? "0" + segundo : segundo;
+
+    // Enviamos la hora a la vista HTML 
+    var tiempo = hora + ":" + minuto + ":" + segundo + " " + meridiano;    
+    document.getElementById("relojnumerico").innerText = tiempo;
+    document.getElementById("relojnumerico").textContent = tiempo;
+
+    // Cargamos el reloj a los 500 milisegundos 
+    setTimeout(cargarReloj, 500);
+
+}
+
+// Ejecutamos la función 'CargarReloj' 
+cargarReloj(); 
